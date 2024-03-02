0f339b4bd0f953d91a15d0f5d1210ad03f0866ce
Roman <roman.kulyk@globallogic.com>
2024-03-02 23:57:22 +0200

    updated task_3.py file to be consistent with pep-8

--- task_3.py
+++ task_3.py
@@ -2,23 +2,23 @@
 1 - Create a function to add a new student to the dictionary. The function
 should take the student's name and their grades for subject as input and
 then add them to dictionary.
-2 - Create a function to calculate a student's average grade. The function should take
-student's name as input and return the average grade for all subjects.
+2 - Create a function to calculate a student's average grade. The function should 
+take student's name as input and return the average grade for all subjects.
 3 - Create a function to remove a student from the dictionary. The function
 should take the student's name as input and remove them from dictionary.
 """
 
 
-students = {"Alice":[85, 90, 92],
-            "Bob":[70, 80, 75],
-            "Charlie":[60, 65, 70]}
+students = {"Alice": [85, 90, 92],
+            "Bob": [70, 80, 75],
+            "Charlie": [60, 65, 70]}
 
 
-def func_to_add(name,*args):
+def func_to_add(name, *args):
     """This is a function to add student to dictionary."""
     students[name] = list(args)
     print(name, "was added to the students dictionary.")
-    
+
 
 def func_to_calculate(name):
     """This is a function to calculate student's average grade."""
@@ -33,9 +33,9 @@ def func_to_remove(name):
     print(name, "was removed from students dictionary.")
 
 
-print(students) 
-func_to_add("Piotr",40,50,60)
-func_to_add("Pawel",58,58,49)
+print(students)
+func_to_add("Piotr", 40, 50, 60)
+func_to_add("Pawel", 58, 58, 49)
 func_to_remove("Bob")
 
 print(students)
