539fe6824149f680ad5108dc27d88160014aad7f
Roman <roman.kulyk@globallogic.com>
2024-03-03 00:01:33 +0200

    removed trailing spaced at lines 5 & 6

---
+++ notes&questions.py
@@ -0,0 +1,9 @@
+message = " Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+In dictum, sem et vestibulum tempus, \
+nunc dui elementum dolor, vitae lobortis nulla nisi consequat ligula.\
+Duis a venenatis urna. Sed eu tincidunt ipsum.\
+Curabitur aliquam velit vitae purus finibus iaculis. Suspendisse \
+sollicitudin tortor sapien, vitae convallis felis \
+rutrum quis. Sed eget nibh sagittis, viverra nibh \
+non, efficitur mi. Phasellus sagittis sagittis nisl, id posuere"
+print(message)
