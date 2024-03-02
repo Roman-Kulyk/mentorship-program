cb6e0d4a90e4676957503f487a1f084764406f4b
Roman <roman.kulyk@globallogic.com>
2024-03-03 00:06:23 +0200

    pep-8 compliance changes

--- it_specialists.py
+++ it_specialists.py
@@ -1,37 +1,37 @@
 employees = {
-            "Emily Williams":"Trainee Devops",
-            "Grace Carter":"Senior HR Specialist",
-            "Ella Patel":"Senior Developer",
-            "Amella Kim":"Senior QA Manual",
-            "David Chang":"Senior QA Automation",
-            "Mia Torres":"Devops",
-            "Sophia Hernandez":"Junior Devops",
-            "Matthew Rodriguez":"Junior Developer",
-            "Joseph Liu":"Senior Manager",
-            "Charlotte Adams":"QA Automation",
-            "Olivia Garcia":"Trainee Manager",
-            "John Doe":"Trainee QA Manual",
-            "Ava Gonzalea":"Junior Manager",
-            "Roman Kulyk":"QA Automation",
-            "Isabella Flores":"HR Specialist",
-            "James Lee":"Junior QA Manual",
-            "Alexander Nguyen":"Manager",
-            "William Lopez":"Junior HR Specialist",
-            "Michael Johnson":"Trainee Developer",
-            "Benjamin Perez":"QA Manual",
-            "Alice Smith":"Trainee QA Automation",
-            "Logan Mitchell":"Senior Devops",
-            "Emma Martinez":"Junior QA Automation",
-            "Daniel Brown":"Trainee HR Specialist",
-            "Ethan Wilson":"Developer"
+            "Emily Williams": "Trainee Devops",
+            "Grace Carter": "Senior HR Specialist",
+            "Ella Patel": "Senior Developer",
+            "Amella Kim": "Senior QA Manual",
+            "David Chang": "Senior QA Automation",
+            "Mia Torres": "Devops",
+            "Sophia Hernandez": "Junior Devops",
+            "Matthew Rodriguez": "Junior Developer",
+            "Joseph Liu": "Senior Manager",
+            "Charlotte Adams": "QA Automation",
+            "Olivia Garcia": "Trainee Manager",
+            "John Doe": "Trainee QA Manual",
+            "Ava Gonzalea": "Junior Manager",
+            "Roman Kulyk": "QA Automation",
+            "Isabella Flores": "HR Specialist",
+            "James Lee": "Junior QA Manual",
+            "Alexander Nguyen": "Manager",
+            "William Lopez": "Junior HR Specialist",
+            "Michael Johnson": "Trainee Developer",
+            "Benjamin Perez": "QA Manual",
+            "Alice Smith": "Trainee QA Automation",
+            "Logan Mitchell": "Senior Devops",
+            "Emma Martinez": "Junior QA Automation",
+            "Daniel Brown": "Trainee HR Specialist",
+            "Ethan Wilson": "Developer"
             }
 
-hr_specialists = {"trainee":[],"junior":[],"middle":[],"senior":[]}
-devops = {"trainee":[],"junior":[],"middle":[],"senior":[]}
-developers = {"trainee":[],"junior":[],"middle":[],"senior":[]}
-qa_manual = {"trainee":[],"junior":[],"middle":[],"senior":[]}
-qa_automation = {"trainee":[],"junior":[],"middle":[],"senior":[]}
-managers = {"trainee":[],"junior":[],"middle":[],"senior":[]}
+hr_specialists = {"trainee": [],"junior": [],"middle": [],"senior": []}
+devops = {"trainee": [],"junior": [],"middle": [],"senior": []}
+developers = {"trainee": [],"junior": [],"middle": [],"senior": []}
+qa_manual = {"trainee": [],"junior": [],"middle": [],"senior": []}
+qa_automation = {"trainee": [],"junior": [],"middle": [],"senior": []}
+managers = {"trainee": [],"junior": [],"middle": [],"senior": []}
 
 for employee, position in employees.items():
     grade = ''
@@ -59,7 +59,7 @@ for employee, position in employees.items():
 
 print('HR = ', hr_specialists)
 print('Devops = ', devops)
-print('Developer = ',developers)
+print('Developer = ', developers)
 print('QA Manual = ', qa_manual)
 print('QA Automation = ', qa_automation)
 print('Manager = ', managers)
