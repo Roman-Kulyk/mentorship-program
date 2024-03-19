ae0ac72fa6389cee2230079d3df4548260e3ddbc
Roman <roman.kulyk@globallogic.com>
2024-03-19 23:32:53 +0200

    added f_strings.py file

---
+++ session_5/f_strings.py
@@ -0,0 +1,20 @@
+name = 'David'
+print(f'My name is {name}')
+
+
+a = 12
+b = 8
+print(f'12 + 8 = {a + b}')
+
+
+price = 250.67890
+print(f'your account: {price:.3f}')
+
+
+value = 4 * 20
+# 1 option
+print('The value is {value}.'.format(value = value))
+# 2 option
+print('The value is {}.'.format(value))
+# 3 option
+print(f'The value is {value}.')
