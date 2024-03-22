f16eba636abcb573cb59d80ef466a50c48ee6336
Roman <roman.kulyk@globallogic.com>
2024-03-22 10:58:56 +0200

    added decorators_1.py file

---
+++ session_6/decorators_1.py
@@ -0,0 +1,21 @@
+def make_pretty(func):
+    def inner():
+        print('I got decorated')
+        func()
+    return inner
+
+
+# def ordinary():
+#     print('I am ordinary')
+
+
+# decorated_func = make_pretty(ordinary)  # it creates a decorator function
+# decorated_func()  # it calls a decorator function
+
+
+@make_pretty
+def ordinary():
+    print('I am ordinary')
+
+
+ordinary()
