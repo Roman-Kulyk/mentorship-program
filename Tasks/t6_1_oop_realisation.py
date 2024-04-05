664e7d5d973e2bc60c9faa28d635ccf3c0f67173
Roman <roman.kulyk@globallogic.com>
2024-04-05 23:35:57 +0300

    added implementation of Abstraction to the Animals class

--- Tasks/t6_1_oop_realisation.py
+++ Tasks/t6_1_oop_realisation.py
@@ -3,33 +3,46 @@ Animalia. With few exceptions, animals consume organic material, breathe
 oxygen, have myocytes and are able ot move, can reproduce sexually, and grow
 from a hollow sphere of cells, the blastuel, during embryonic development.
 """
-class Animals:
-# Here we have first OOP paradigm Abstraction to describe main animal's function
+from abc import ABC
+
+
+class Animals(ABC):
+    # Here we have first OOP paradigm Abstraction to describe main animal's 
+    # function
     def __init__(self, name, age):
         # It is attribute's initialization
         self._name = name  # here encapsulation is used to restrict access
         self._age = age    # to attributes only from subclasses
+        super().__init__()
 
     def eat(self):
-        print('I like to eat!')
+        pass
 
     def breathe(self):
-        print("I can breathe only air!")
-        
+        pass
+
     def move(self):
-        print('I Like to Move It, Move It!!!')
+        pass
 
     def reproduce(self):
-        print('It is a main goal of our life!')
+        pass
 
 
-# It is an Inheritance paradigm, all following sublasses derived from main class
-class Mammals(Animals):  
+# It's an Inheritance paradigm, all following sublasses derived from main class
+class Mammals(Animals):
     def move(self):
         print('I can run!!!')
 
     def reproduce(self):
         print('We give birth to live babies.')
+
+    def breathe(self):
+        print("I can breathe only air though my lungs!")
+
+    def eat(self):
+        print('I like to eat grass!')
+
+
 # It is a Polymorphism paradigem which allow to us have method with the same
 # name in different subclasse which inhereted the method from main class
 class Reptiles(Animals):
@@ -39,12 +52,26 @@ class Reptiles(Animals):
     def reproduce(self):
         print('We produce eggs in the nest.')
 
+    def breathe(self):
+        print("I can breathe only air though my lungs!")
+
+    def eat(self):
+        print('I like to eat insects, molusks, frogs!')
+
+
 class Fish(Animals):
     def move(self):
         print('I can swim!!!')
 
     def reproduce(self):
         print('We are considered to be live-bearing')
+
+    def breathe(self):
+        print("I can breathe only air from water!")
+
+    def eat(self):
+        print('I like to eat plankton!')
+
 
 class Birds(Animals):
     def move(self):
@@ -62,7 +89,8 @@ class Invertebrates(Animals):
 class Insects(Animals):
     pass
 
-# It is here to show that attributes are variable which can be accessed throuh 
+
+# It is here to show that attributes are variable which can be accessed throuh
 # instances of class
 my_pet1 = Mammals('Oskar', 2.5)
 my_pet2 = Fish('Goldy', 0.5)
