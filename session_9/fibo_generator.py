44620840002fadadba7d4018f695fdecc49caef0
Roman <roman.kulyk@globallogic.com>
2024-05-15 10:31:28 +0300

    added another option from another resource

--- session_9/fibo_generator.py
+++ session_9/fibo_generator.py
@@ -1,39 +1,92 @@
-def counting_out(max_count):
-    count = 1
-    while count <= max_count:
-        yield count
-        count += 1
-
-
-for i in counting_out(5): print(i)
-
-
-def fibo_generator(max_count):
+###############################################################################
+def fibo_generator(number):
     """This is a function that generates list of Fibonacci numbers."""
-    if max_count >= 1:
+    if number >= 1:
         yield 1
-    if max_count >= 2:
+    if number >= 2:
         yield 1
-    first, second = 1, 2
+    n1, n2 = 1, 2
     count = 3
-    while count <= max_count:
-        yield second
-        first, second = second, first + second
+    while count <= number:
+        yield n2
+        n1, n2 = n2, n1 + n2
         count += 1
-
-
-f_gen = fibo_generator(25)
-print(list(f_gen))
-
-
-def fibo_generator(max_count):
-    """This is a function that generates list of Fibonacci numbers."""
-    first, second = 0, 1
-    for _ in range(max_count):
-        yield second
-        first, second = second, first + second
 
 
 f_gen = fibo_generator(10)
-print(type(f_gen))
+print(f_gen)
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+#print(next(f_gen))
 print(list(f_gen))
+print()
+
+###############################################################################
+def fibo_generator(number):
+    """This is a function that generates list of Fibonacci numbers."""
+    n1, n2 = 0, 1
+    for _ in range(number):
+        yield n2
+        n1, n2 = n2, n1 + n2
+
+
+f_gen = fibo_generator(10)
+print(f_gen)
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+# print(next(f_gen))
+#print(next(f_gen))
+print(list(f_gen))
+print()
+
+###############################################################################
+class IteratorFib():
+    """This is a class that generates list of Fibonacci numbers."""
+    def __init__(self, number):
+        self.n1 = 0
+        self.n2 = 1
+        self.c = 0
+        self.number = number
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.c == 0:
+            self.c += 1
+            return 0
+        self.n1, self.n2 = self.n2, self.n1 + self.n2
+        self.c += 1
+        if self.c > self.number:
+            raise StopIteration
+        return self.n1
+    
+fib1 = IteratorFib(10)
+print(fib1)
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+# print(next(fib1))
+print(list(fib1))
