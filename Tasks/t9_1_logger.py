3fda436746aa9beace815e46a5c6a37cde399614
Roman <roman.kulyk@globallogic.com>
2024-05-23 00:48:44 +0300

    updated t9_1_logger.py filearter Peer review

--- Tasks/t9_1_logger.py
+++ Tasks/t9_1_logger.py
@@ -1,11 +1,6 @@
 import time
 
 
-time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-time_str_file = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-start_time = time.time()
-
-
 class Logger():
     """
     This is a class that defines Logger object.
@@ -13,10 +8,20 @@ class Logger():
     logfile_name: str
         name of log file
     """
+    count_alerts = 0
+    count_warnings = 0
 
-    def __init__(self, logfile_name=(f"{time_str_file}.log")):
+    def __init__(self, logfile_name=None):
         """Constructs all necessary attributes for the Logger object."""
+        self.time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+        self.time_str_file = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
+        self.start_time = time.time()
+        if logfile_name is None:
+            logfile_name = (f"{self.time_str_file}.log")
         self.logfile_name = logfile_name
+
+        with open(self.logfile_name, 'w') as freading:
+            print('\n')
 
     def show_log_banner(self, message) -> str:
         """
@@ -25,12 +30,12 @@ class Logger():
         with open(self.logfile_name, 'a') as freading:
             freading.write('='*80)
             freading.write("\n")
-            freading.write(f'{time_str}:{message}')
+            freading.write(f'{self.time_str}:{message}')
             freading.write("\n")
             freading.write('='*80)
             freading.write("\n")
         print('='*80)
-        print(f'{time_str}:{message}')
+        print(f'{self.time_str}:{message}')
         print('='*80)
 
     def show_log_header(self, message) -> str:
@@ -38,12 +43,12 @@ class Logger():
         This method shows header-message into terminal and logs it into a file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f'{time_str}:{message}')
+            freading.write(f'{self.time_str}:{message}')
             freading.write("\n")
             freading.write('-'*80)
             freading.write("\n")
 
-        print(f'{time_str}:{message}')
+        print(f'{self.time_str}:{message}')
         print('-'*80)
 
     def log_banner(self, message) -> str:
@@ -51,7 +56,7 @@ class Logger():
         with open(self.logfile_name, 'a') as freading:
             freading.write('='*80)
             freading.write("\n")
-            freading.write(f'{time_str}:{message}')
+            freading.write(f'{self.time_str}:{message}')
             freading.write("\n")
             freading.write('='*80)
             freading.write("\n")
@@ -59,7 +64,7 @@ class Logger():
     def log_header(self, message) -> str:
         """This method logs header-messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f'{time_str}:{message}')
+            freading.write(f'{self.time_str}:{message}')
             freading.write("\n")
             freading.write('-'*80)
             freading.write("\n")
@@ -70,9 +75,9 @@ class Logger():
         into a file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[INIT]'}{message}")
+            freading.write(f"{self.time_str}:{'[INIT]'}{message}")
             freading.write("\n")
-        print(f"{time_str}:{'[INIT]'}{message}")
+        print(f"{self.time_str}:{'[INIT]'}{message}")
 
     def show_log_action(self, message) -> str:
         """
@@ -80,9 +85,9 @@ class Logger():
         file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[ACTION]'}{message}")
+            freading.write(f"{self.time_str}:{'[ACTION]'}{message}")
             freading.write("\n")
-        print(f"{time_str}:{'[ACTION]'}{message}")
+        print(f"{self.time_str}:{'[ACTION]'}{message}")
 
     def show_log_warning(self, message) -> str:
         """
@@ -90,9 +95,10 @@ class Logger():
         file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[WARNING]'}{message}")
+            freading.write(f"{self.time_str}:{'[WARNING]'}{message}")
             freading.write("\n")
-        print(f"{time_str}:{'[WARNING]'}{message}")
+        print(f"{self.time_str}:{'[WARNING]'}{message}")
+        self.count_warnings += 1
 
     def show_log_alert(self, message) -> str:
         """
@@ -100,9 +106,10 @@ class Logger():
         file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[ALERT]'}{message}")
+            freading.write(f"{self.time_str}:{'[ALERT]'}{message}")
             freading.write("\n")
-        print(f"{time_str}:{'[ALERT]'}{message}")
+        print(f"{self.time_str}:{'[ALERT]'}{message}")
+        self.count_alerts += 1
 
     def show_log_info(self, message) -> str:
         """
@@ -110,123 +117,60 @@ class Logger():
         file.
         """
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[INFO]'}{message}")
+            freading.write(f"{self.time_str}:{'[INFO]'}{message}")
             freading.write("\n")
-        print(f"{time_str}:{'[INFO]'}{message}")
+        print(f"{self.time_str}:{'[INFO]'}{message}")
 
     def log_init(self, message) -> str:
         """This method logs initialization messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[INIT]'}{message}")
+            freading.write(f"{self.time_str}:{'[INIT]'}{message}")
             freading.write("\n")
 
     def log_action(self, message) -> str:
         """This method logs action messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[ACTION]'}{message}")
+            freading.write(f"{self.time_str}:{'[ACTION]'}{message}")
             freading.write("\n")
 
     def log_warning(self, message) -> str:
         """This method logs warning messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:'[WARNING]'{message}")
+            freading.write(f"{self.time_str}:'[WARNING]'{message}")
             freading.write("\n")
+        self.count_warnings += 1
 
     def log_alert(self, message) -> str:
         """This method logs alert messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[ALERT]'}{message}")
+            freading.write(f"{self.time_str}:{'[ALERT]'}{message}")
             freading.write("\n")
+        self.count_alerts += 1
 
     def log_info(self, message) -> str:
         """This method logs info messages into a file."""
         with open(self.logfile_name, 'a') as freading:
-            freading.write(f"{time_str}:{'[INFO]'}{message}")
+            freading.write(f"{self.time_str}:{'[INFO]'}{message}")
             freading.write("\n")
 
     def report(self) -> None:
         """This method summarizes and reports all the activities."""
+        finish_time = time.time()
 
-        with open(self.logfile_name, 'r') as freading:
-            warning_value = 0
-            alert_value = 0
-            lines = freading.readlines()
-            for i in lines:
-                if '[WARNING]' in i:
-                    warning_value += 1
-            for i in lines:
-                if '[ALERT]' in i:
-                    alert_value += 1
+        with open(self.logfile_name, 'a') as freading:
+            freading.write(f"{self.time_str}:")
+            freading.write("\n")
+            freading.write(f"{self.time_str}:{'='*60}")
+            freading.write("\n")
+            freading.write(f"{self.time_str}:{'Total alerts:'} {self.count_alerts}")
+            freading.write("\n")
+            freading.write(f"{self.time_str}:{'Total warning:'} {self.count_warnings}")
+            freading.write("\n")
+            elapsed_time_sec = finish_time - self.start_time
+            freading.write(f"{self.time_str}:Time spent: {elapsed_time_sec:.4f}")
 
-            if self.logfile_name == (f"{time_str_file}.log"):
-                with open(self.logfile_name, 'a') as freading:
-                    freading.write(f"{time_str}:")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'='*60}")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'Total alerts:'} {alert_value}")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'Total warning:'} {warning_value}")
-                    freading.write("\n")
-                    elapsed_time_sec = finish_time - start_time
-                    freading.write(f"{time_str}:Time spent: {elapsed_time_sec:.4f}")
-
-            else:
-                with open(self.logfile_name, 'w') as freading:
-                    freading.write(f"{time_str}:")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'='*60}")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'Total alerts:'} {alert_value}")
-                    freading.write("\n")
-                    freading.write(f"{time_str}:{'Total warning:'} {warning_value}")
-                    freading.write("\n")
-                    elapsed_time_sec = finish_time - start_time
-                    freading.write(f"{time_str}:Time spent: {elapsed_time_sec:.4f}")
-
-                print(f"{time_str}:")
-                print(f"{time_str}:{'='*60}")
-                print(f"{time_str}:{'Total alerts:'} {alert_value}")
-                print(f"{time_str}:{'Total warning:'} {warning_value}")
-                print(f"{time_str}:Time spent:{elapsed_time_sec:.4f}")
-
-
-time.sleep(2)
-finish_time = time.time()
-
-
-logger_1 = Logger()  # Generates file with name 2024-05-18-04-32.log
-
-logger_1.show_log_banner("Hello Joe!")
-logger_1.show_log_header("Good Bye Joe!")
-
-logger_1.log_banner("Hello R2D2!")
-logger_1.log_header("Goodbye R2D2!'")
-# What is the correct way to put string on the second line?
-logger_1.show_log_init("This initialization message is generated by " +\
-                       "show_log_init method")
-logger_1.show_log_action("This action message is generated by " +\
-                         "show_log_action method")
-logger_1.show_log_warning("This warning message is generated by " +\
-                          "show_log_warning method")
-logger_1.show_log_alert("This alert message is generated by " +\
-                        "show_log_alert method")
-logger_1.show_log_info("This info message is generated by " +\
-                       "show_log_info method")
-
-logger_1.log_init("This initialization message is generated by "\
-                  "log_init method")
-logger_1.log_action("This action message is generated by "\
-                    "log_action method")
-logger_1.log_warning("This warning message is generated by "\
-                     "log_warning method")
-logger_1.log_alert("This alert message is generated by "\
-                   "log_alert method")
-logger_1.log_info("This info message is generated by "\
-                  "log_info method")
-logger_1.report()
-print()
-logger_2 = Logger('my_logfile.log')  # Generates file with name my_logfile.log
-logger_2.log_info("This info message is generated by "\
-                  "log_info method")
-logger_2.report()
+        print(f"{self.time_str}:")
+        print(f"{self.time_str}:{'='*60}")
+        print(f"{self.time_str}:{'Total alerts:'} {self.count_alerts}")
+        print(f"{self.time_str}:{'Total warning:'} {self.count_warnings}")
+        print(f"{self.time_str}:Time spent:{elapsed_time_sec:.4f}")
