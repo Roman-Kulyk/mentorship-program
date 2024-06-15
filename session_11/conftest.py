698e439d2bf6fc4236db35a9cc0d5d237c107016
Roman <roman.kulyk@globallogic.com>
2024-06-15 17:18:32 +0300

    just need to commit the changes

--- session_11/conftest.py
+++ session_11/conftest.py
@@ -1,10 +1,9 @@
-# content of conftest.py
-from test_foocompare import Foo
+# Content of conftest_1.py
+import smtplib
+
+import pytest
 
 
-def pytest_assertrepr_compare(op, left, right):
-    if isinstance(left, Foo) and isinstance(right, Foo) and op == "==":
-        return[
-            "Comparing Foo instances:"
-            f" vals: {left.val} != {right.val}",
-        ]
+@pytest.fixture(scope="module")
+def smtp_connection():
+    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
--- session_11/conftest_1.py
+++
@@ -1,9 +0,0 @@
-# Content of conftest_1.py
-import smtplib
-
-import pytest
-
-
-@pytest.fixture(scope="module")
-def smtp_connection():
-    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
