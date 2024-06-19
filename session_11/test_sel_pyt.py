0d0ed58b93a915613dd381a43259bc0ed97455aa
Roman <roman.kulyk@globallogic.com>
2024-06-19 18:35:27 +0300

    added comments to the test_sel_pyt.py file

--- session_11/test_sel_pyt.py
+++ session_11/test_sel_pyt.py
@@ -8,26 +8,37 @@ import pytest
 # Arrange
 @pytest.fixture(scope='session')
 def driver():
+    # Driver initialization
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
 
 def test_func_1(driver):
+    # Act
+    # Open certain web page
     driver.get("https://www.python.org")
     driver.implicitly_wait(3)
+    # Find websearch element by class name
     search_input = driver.find_element(By.CLASS_NAME, "search-field")
     search_input.clear()
 
 
 def test_func_2(driver):
+    # Act
+    # Open certain web page
     driver.get("https://duckduckgo.com")
     print(driver.title)
+    # Find websearch element by name
     search_input = driver.find_element("name", "q")
+    # Enter search text
     search_input.send_keys("Selenium")
+    # Emulate press the Enter button
     search_input.send_keys(Keys.ENTER)
 
 
 def test_func_3(driver):
+    # Act
+    # Open certain web page
     driver.get("https://www.uefa.com")
     driver.implicitly_wait(3)
