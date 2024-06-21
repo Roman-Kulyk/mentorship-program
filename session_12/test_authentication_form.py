80448d6ba2e76bd23f6b83680feafdfdd8fc9468
Roman <roman.kulyk@globallogic.com>
2024-06-21 08:21:32 +0300

    added window maximization

--- session_12/test_authentication_form.py
+++ session_12/test_authentication_form.py
@@ -15,25 +15,28 @@ def driver():
 
 
 def test_successful_auth(driver):
-
+    driver.maximize_window()
     # Open certain web page
     driver.get("https://www.saucedemo.com/v1/")
     driver.implicitly_wait(3)
 
-    # Find websearch element by id
-    user_name_input = driver.find_element(By.ID, "user-name")
+    # Find websearch element by id or XPATH
+    #user_name_input = driver.find_element(By.ID, "user-name")
+    user_name_input = driver.find_element(By.XPATH, "//input[@data-test='username']")
     user_name_input.clear()
     # Enter user name
     user_name_input.send_keys("standard_user")
 
-    # Find websearch element by id
-    password_input = driver.find_element(By.ID, "password")
+    # Find websearch element by id or XPATH
+    #password_input = driver.find_element(By.ID, "password")
+    password_input = driver.find_element(By.XPATH, "//input[@data-test='password']")
     password_input.clear()
     # Enter user name
     password_input.send_keys("secret_sauce")
 
-    # Find websearch element by id
-    login_button = driver.find_element(By.ID, "login-button")
+    # Find websearch element by id or XPATH
+    #login_button = driver.find_element(By.ID, "login-button")
+    login_button = driver.find_element(By.XPATH, "//input[@value='LOGIN']")
     # Emulate press the Enter button
     login_button.send_keys(Keys.RETURN)
-
+    print(driver.title)
