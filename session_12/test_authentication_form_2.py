dd5520c37a65c42165599863b90c725a796924e0
Roman <roman.kulyk@globallogic.com>
2024-06-23 10:07:34 +0300

    deleted unneccessery imports from files

--- session_12/test_authentication_form_2.py
+++ session_12/test_authentication_form_2.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 import pytest
 
