0a26d7851b8665d133b3c17d27da69ae1d8f105d
Roman <roman.kulyk@globallogic.com>
2024-06-24 18:13:54 +0300

    save unsaved changes

--- Tasks/t12_test_authentication_form.py
+++ Tasks/t12_test_authentication_form.py
@@ -7,146 +7,150 @@ from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
 
-# Arrange
-@pytest.fixture(scope='session')
-def driver():
-    # Driver initialization
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+class LoginPage:
+
+    # Arrange
+    @pytest.fixture(scope='session')
+    def driver():
+        # Driver initialization
+        driver = webdriver.Chrome()
+        yield driver
+        driver.quit()
 
 
-def test_successful_auth(driver):
-    driver.maximize_window()
-    # Open certain web page
-    driver.get("https://www.saucedemo.com/v1/")
-    driver.implicitly_wait(3)
+    def test_successful_auth(driver):
+        driver.maximize_window()
+        # Open certain web page
+        driver.get("https://www.saucedemo.com/v1/")
+        driver.implicitly_wait(3)
 
-    # Find websearch element by ID or XPATH
-    # user_name_input = driver.find_element(By.ID, "user-name")
-    user_name_input = driver.find_element(By.XPATH,
-                                          "//input[@data-test='username']")
-    user_name_input.clear()
-    # Enter user name
-    user_name_input.send_keys("standard_user")
+        # Find websearch element by ID or XPATH
+        # user_name_input = driver.find_element(By.ID, "user-name")
+        user_name_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='username']")
+        user_name_input.clear()
+        # Enter user name
+        user_name_input.send_keys("standard_user")
 
-    # Find websearch element by ID or XPATH
-    # password_input = driver.find_element(By.ID, "password")
-    password_input = driver.find_element(By.XPATH,
-                                         "//input[@data-test='password']")
-    password_input.clear()
-    # Enter user name
-    password_input.send_keys("secret_sauce")
+        # Find websearch element by ID or XPATH
+        # password_input = driver.find_element(By.ID, "password")
+        password_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='password']")
+        password_input.clear()
+        # Enter user name
+        password_input.send_keys("secret_sauce")
 
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                       "//input[@value='LOGIN']")
-    # Emulate press the Enter button
-    login_button.send_keys(Keys.RETURN)
-    # Use an explicit wait with an expected condition to tell when one of your
-    # logine elements has gone stale
-    WebDriverWait(driver, 10).until(EC.staleness_of(user_name_input))
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//input[@value='LOGIN']")
+        # Emulate press the Enter button
+        login_button.send_keys(Keys.RETURN)
+        # Use an explicit wait with an expected condition to tell when one of your
+        # logine elements has gone stale
+        WebDriverWait(driver, 10).until(EC.staleness_of(user_name_input))
 
 
-def test_unsuccessful_auth(driver):
-    driver.maximize_window()
-    # Open certain web page
-    driver.get("https://www.saucedemo.com/v1/")
-    driver.implicitly_wait(3)
-    
-    # Find websearch element by ID or XPATH
-    # user_name_input = driver.find_element(By.ID, "user-name")
-    user_name_input = driver.find_element(By.XPATH,
-                                        "//input[@data-test='username']")
-    user_name_input.clear()
-    # Enter user name
-    user_name_input.send_keys("wrong_standard_user")
+    def test_unsuccessful_auth(driver):
+        driver.maximize_window()
+        # Open certain web page
+        driver.get("https://www.saucedemo.com/v1/")
+        driver.implicitly_wait(3)
+        
+        # Find websearch element by ID or XPATH
+        # user_name_input = driver.find_element(By.ID, "user-name")
+        user_name_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='username']")
+        user_name_input.clear()
+        # Enter user name
+        user_name_input.send_keys("wrong_standard_user")
 
-    # Find websearch element by ID or XPATH
-    # password_input = driver.find_element(By.ID, "password")
-    password_input = driver.find_element(By.XPATH,
-                                        "//input[@data-test='password']")
-    password_input.clear()
-    # Enter user name
-    password_input.send_keys("secret_sauce")
+        # Find websearch element by ID or XPATH
+        # password_input = driver.find_element(By.ID, "password")
+        password_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='password']")
+        password_input.clear()
+        # Enter user name
+        password_input.send_keys("secret_sauce")
 
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                    "//input[@value='LOGIN']")
-    # Emulate press the Enter button
-    login_button.send_keys(Keys.RETURN)
-    
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                    "//h3[@data-test='error']")
-    
-def test_unsuccessful_auth_wp(driver):
-    driver.maximize_window()
-    # Open certain web page
-    driver.get("https://www.saucedemo.com/v1/")
-    driver.implicitly_wait(3)
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//input[@value='LOGIN']")
+        # Emulate press the Enter button
+        login_button.send_keys(Keys.RETURN)
+        
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//h3[@data-test='error']")
+        
 
-    # Find websearch element by ID or XPATH
-    # user_name_input = driver.find_element(By.ID, "user-name")
-    user_name_input = driver.find_element(By.XPATH,
-                                        "//input[@data-test='username']")
-    user_name_input.clear()
-    # Enter user name
-    user_name_input.send_keys("standard_user")
+    def test_unsuccessful_auth_wp(driver):
+        driver.maximize_window()
+        # Open certain web page
+        driver.get("https://www.saucedemo.com/v1/")
+        driver.implicitly_wait(3)
 
-    # Find websearch element by ID or XPATH
-    # password_input = driver.find_element(By.ID, "password")
-    password_input = driver.find_element(By.XPATH,
-                                        "//input[@data-test='password']")
-    password_input.clear()
-    # Enter user name
-    password_input.send_keys("wrong_secret_sauce")
+        # Find websearch element by ID or XPATH
+        # user_name_input = driver.find_element(By.ID, "user-name")
+        user_name_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='username']")
+        user_name_input.clear()
+        # Enter user name
+        user_name_input.send_keys("standard_user")
 
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                    "//input[@value='LOGIN']")
-    # Emulate press the Enter button
-    login_button.send_keys(Keys.RETURN)
-    
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                    "//h3[@data-test='error']")
+        # Find websearch element by ID or XPATH
+        # password_input = driver.find_element(By.ID, "password")
+        password_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='password']")
+        password_input.clear()
+        # Enter user name
+        password_input.send_keys("wrong_secret_sauce")
 
-def test_unsuccessful_auth_wunp(driver):
-    driver.maximize_window()
-    # Open certain web page
-    driver.get("https://www.saucedemo.com/v1/")
-    driver.implicitly_wait(3)
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//input[@value='LOGIN']")
+        # Emulate press the Enter button
+        login_button.send_keys(Keys.RETURN)
+        
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//h3[@data-test='error']")
 
-    # Find websearch element by ID or XPATH
-    # user_name_input = driver.find_element(By.ID, "user-name")
-    user_name_input = driver.find_element(By.XPATH,
-                                          "//input[@data-test='username']")
-    user_name_input.clear()
-    # Enter user name
-    user_name_input.send_keys("wrong_standard_user")
 
-    # Find websearch element by ID or XPATH
-    # password_input = driver.find_element(By.ID, "password")
-    password_input = driver.find_element(By.XPATH,
-                                         "//input[@data-test='password']")
-    password_input.clear()
-    # Enter user name
-    password_input.send_keys("wrong_secret_sauce")
+    def test_unsuccessful_auth_wunp(driver):
+        driver.maximize_window()
+        # Open certain web page
+        driver.get("https://www.saucedemo.com/v1/")
+        driver.implicitly_wait(3)
 
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                       "//input[@value='LOGIN']")
-    # Emulate press the Enter button
-    login_button.send_keys(Keys.RETURN)
-    
-    # Find websearch element by ID or XPATH
-    # login_button = driver.find_element(By.ID, "login-button")
-    login_button = driver.find_element(By.XPATH,
-                                       "//h3[@data-test='error']")
+        # Find websearch element by ID or XPATH
+        # user_name_input = driver.find_element(By.ID, "user-name")
+        user_name_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='username']")
+        user_name_input.clear()
+        # Enter user name
+        user_name_input.send_keys("wrong_standard_user")
+
+        # Find websearch element by ID or XPATH
+        # password_input = driver.find_element(By.ID, "password")
+        password_input = driver.find_element(By.XPATH,
+                                            "//input[@data-test='password']")
+        password_input.clear()
+        # Enter user name
+        password_input.send_keys("wrong_secret_sauce")
+
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//input[@value='LOGIN']")
+        # Emulate press the Enter button
+        login_button.send_keys(Keys.RETURN)
+        
+        # Find websearch element by ID or XPATH
+        # login_button = driver.find_element(By.ID, "login-button")
+        login_button = driver.find_element(By.XPATH,
+                                        "//h3[@data-test='error']")
