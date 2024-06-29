c28b011182e2acaac3102a6570dd655243ae428d
Roman <roman.kulyk@globallogic.com>
2024-06-29 18:18:15 +0300

    added test_fixture_marks.py file

---
+++ session_12/test_fixture_marks.py
@@ -0,0 +1,11 @@
+# content of test_fixture_marks.py
+import pytest
+
+
+@pytest.fixture(params=[0, 1, pytest.param(2, marks=pytest.mark.skip)])
+def data_set(request):
+    return request.param
+
+
+def test_data(data_set):
+    pass
