67489dd121c4e3b0e7c36c2cf25161de4d829460
Roman Kulyk <roman.kulyk@globallogic.com>
2024-07-28 17:22:25 +0300

    added another copy of folder for experiments

---
+++ my_tests_2/pages/cart_page.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+from my_tests.lib.constants import *
+from my_tests.pages.main_page import MainPage
+
+class CartPage(MainPage):
+    pass
---
+++ my_tests_2/tests/test_login_pom.py
@@ -0,0 +1,44 @@
+import pytest
+from my_tests.pages.login_page import LoginPage
+from my_tests.lib.constants import *
+import time
+
+@pytest.mark.parametrize('user_input,password,is_valid',
+                             [('standard_user', 'secret_sauce', True),
+                              ('', '', False),
+                              ('', 'secret_sauce', False),
+                              ('standard_user', '', False),
+                              ('wrong_standard_user', 'secret_sauce', False),
+                              ('standard_user', 'wrong_secret_sauce', False),
+                              ('wrong_standard_user', 'wrong_secret_sauce',
+                               False)])
+
+def test_login_functionality(chrome_browser:object, user_input:str,
+                             password:str, is_valid:bool) -> None:
+    """
+    This is a method to verify login functionality.
+    Parameters
+    chrome_browser:self
+    user_input:str
+               Username to log in with
+    password:str
+               Password to log in with
+    is_valid:bool
+               Variable to declare if log in s/b successfull or not
+    """
+
+    login_page = LoginPage(chrome_browser)
+    # Open Page
+    login_page.open_page(LOGIN_PAGE_URL)
+
+    # Enter Username and Password
+    login_page.enter_username(user_input)
+    login_page.enter_password(password)
+
+    # Click Login
+    login_page.click_login()
+    time.sleep(1)
+
+    # Verify Successful Login by checking the presence of a logout button
+    login_page.verify_successfull_login(is_valid)
+
