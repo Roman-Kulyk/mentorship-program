7e8e7610618c7c85ab7446bb345aa349937326e2
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-06 12:58:32 +0300

    commit just to do something

--- my_tests_1/lib/constants.py
+++ my_tests_1/lib/constants.py
@@ -1,5 +1,27 @@
+# Login page
+#===============================================================================
 LOGIN_PAGE_URL = "https://www.saucedemo.com/v1/"
 ENTER_USERNAME_XPATH = "//input[@data-test='username']"
 ENTER_PASSWORD = "//input[@data-test='password']"
 CLICK_LOGIN = "//input[@value='LOGIN']"
 ERROR_MESSAGE = "//h3[@data-test='error']"
+
+# Inventory page
+INVENTORY_PAGE_URL = "https://www.saucedemo.com/v1/inventory/"
+#===============================================================================
+# Procuct cards
+
+SL_1 = "(//div[@class = 'inventory_item'])[1]"
+SL_2 = "(//div[@class = 'inventory_item'])[2]"
+SL_3 = "(//div[@class = 'inventory_item'])[3]"
+SL_4 = "(//div[@class = 'inventory_item'])[4]"
+SL_5 = "(//div[@class = 'inventory_item'])[5]"
+SL_6 = "(//div[@class = 'inventory_item'])[6]"
+
+# Others
+SIDE_BAR = "//div[@class = 'bm-menu']"
+SORT_PRODUCT_CARDS = "//select[@class = 'product_sort_container']"
+CART_BUTTON = "//div[@id = 'shopping_cart_container']"
+
+# Cart page
+#===============================================================================
--- my_tests_1/pages/inventory_page.py
+++ my_tests_1/pages/inventory_page.py
@@ -1,6 +1,60 @@
+from selenium.webdriver.common.by import By
 from my_tests_1.lib.constants import *
 from my_tests_1.pages.main_page import MainPage
 
 
 class InventoryPage(MainPage):
-    pass
+    
+    def sl_backpack(self):
+        price = 29.99
+        sauce_labs_backpack = self.driver.find_element(By.XPATH, SL_1)
+                
+    def sl_bike_light(self):
+        price = 9.99
+        sauce_labs_bike_light = self.driver.find_element(By.XPATH, SL_2)
+                
+    def sl_bolt_tshirt(self):
+        price = 15.99
+        sauce_labs_bolt_tshirt = self.driver.find_element(By.XPATH, SL_3)
+
+    def sl_fleese_jacket(self):
+        price = 49.99
+        sauce_labs_fleese_jacket = self.driver.find_element(By.XPATH, SL_4) 
+
+    def sl_onesie(self):
+        price = 7.99
+        sauce_labs_onesie = self.driver.find_element(By.XPATH, SL_5)     
+            
+    def test_all_the_things(self):
+        price = 15.99
+        sauce_labs_test_all_the_things = self.driver.find_element(By.XPATH, SL_6)
+
+
+    
+    
+    # OPTIONAL
+    # def select_item(self, locator, price):
+    #     self.price = price
+    #     self.select_item = self.driver.find_element(By.XPATH,locator)
+    #     self.add_to_cart.click()
+    
+    def add_to_cart(self, locator):
+        add_to_cart_button = self.driver.find_element(By.XPATH,locator)
+        add_to_cart_button.click()
+
+    def go_to_product_page_link(self, locator):
+        self.inventory_item_name = self.driver.find_element(By.XPATH, locator) 
+        self.inventory_item_name.click()
+
+    def side_bar(self):
+        self.bm_menu = self.driver.find_element(By.XPATH, SIDE_BAR)
+        self.bm_menu.click()
+
+    def sort_product_cards(self):
+        self.sort_product_card_link = self.driver.find_element(By.XPATH,
+                                                           SORT_PRODUCT_CARDS)
+        self.sort_product_cards_link.click()
+
+    def cart_button(self):
+        self.shopping_cart_container = self.driver.find_element(By.XPATH, CART_BUTTON)
+        self.shopping_cart_container.click()
