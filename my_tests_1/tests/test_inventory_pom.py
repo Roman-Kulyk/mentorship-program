6cbbe111763ce8ab573cbb793b50a5edf475d8cf
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-07 18:32:27 +0300

    working test finally

--- my_tests_1/tests/test_inventory_pom.py
+++ my_tests_1/tests/test_inventory_pom.py
@@ -1,5 +1,3 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 import pytest
 from my_tests_1.pages.login_page import *
 from my_tests_1.pages.inventory_page import *
@@ -21,8 +19,8 @@ def test_login_functionality(chrome_browser:object, user_input:str,
     is_valid:bool
                Variable to declare if log in s/b successfull or not
     """
-
-    login_page = LoginPage(chrome_browser)
+    driver = chrome_browser
+    login_page = LoginPage(driver)
     # Open Page
     login_page.open_page(LOGIN_PAGE_URL)
 
@@ -32,13 +30,9 @@ def test_login_functionality(chrome_browser:object, user_input:str,
 
     # Click Login
     login_page.click_login()
-    time.sleep(5)
-    # InventoryPage.cart_button()
-    
-    
-def test_cart_page(chrome_browser): 
-    inventory_page = InventoryPage(chrome_browser)
-    # Open Page
-    inventory_page.open_page(INVENTORY_PAGE_URL)
+    time.sleep(3)
+        
+    inventory_page = InventoryPage(driver)
     # Click Cart button
     inventory_page.cart_button()
+    time.sleep(3)
