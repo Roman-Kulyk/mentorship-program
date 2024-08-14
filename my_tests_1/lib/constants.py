66fed208661186a9b6896b3881fe56c48afe4915
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-14 18:51:55 +0300

    added SHOPPING_CART_BADGE

--- my_tests_1/lib/constants.py
+++ my_tests_1/lib/constants.py
@@ -7,7 +7,7 @@ CLICK_LOGIN = "//input[@value='LOGIN']"
 ERROR_MESSAGE = "//h3[@data-test='error']"
 
 # Inventory page
-
+SHOPPING_CART_PAGE = "//span[@class = 'fa-layers-counter shopping_cart_badge']"
 
 #===============================================================================
 # Procuct cards
