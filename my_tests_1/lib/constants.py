9224b266e3390488948390f60013745c68489e3e
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-24 15:15:15 +0300

    typo was fixed

--- my_tests_1/lib/constants.py
+++ my_tests_1/lib/constants.py
@@ -3,7 +3,7 @@
 LOGIN_PAGE_URL = "https://www.saucedemo.com/v1/"
 ENTER_USERNAME_XPATH = "//input[@data-test = 'username']"
 ENTER_PASSWORD = "//input[@data-test = 'password']"
-CLICK_LOGIN = "//input[@value =- 'LOGIN']"
+CLICK_LOGIN = "//input[@value = 'LOGIN']"
 ERROR_MESSAGE = "//h3[@data-test = 'error']"
 # =============================================================================
 # Inventory page
