933edfd5eb64d5b3056e877b4999e7a7af88a43d
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-25 13:36:40 +0300

    pycodestyle fix

--- my_tests_1/pages/chp_3_page.py
+++ my_tests_1/pages/chp_3_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 class CheckoutPage3(MainPage):
-    
+
     def __init__(self, driver):
         """
         This is a method to initialize instance of the CheckoutPage3 class.
@@ -12,15 +12,12 @@ class CheckoutPage3(MainPage):
         webdriver:object
                 webdriver to initialize
         Methods:
-        continue_shopping
-            This is a method to define continue shopping button
-        checkout
-            This is a method to define checkout button
+        pony_express_image
+            This is a method to define pony express image
         """
         super().__init__(driver)
 
     def pony_express_image(self) -> None:
         """This is a method to find and click CONTINUE button on page."""
-        self.pony_express = self.driver.find_element(By.XPATH,
-                                        PONY_EXPRESS)
-        
+        self.pony_express = self.driver.find_element(
+            By.XPATH, PONY_EXPRESS)
