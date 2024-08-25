498f9363ed609c34e549d4275ef0ca7bc356d5f9
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-25 13:33:08 +0300

    pycodestyle fix

--- my_tests_1/pages/chp_2_page.py
+++ my_tests_1/pages/chp_2_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 class CheckoutPage2(MainPage):
-    
+
     def __init__(self, driver):
         """
         This is a method to initialize instance of the CheckoutPage2 class.
@@ -12,21 +12,21 @@ class CheckoutPage2(MainPage):
         webdriver:object
                 webdriver to initialize
         Methods:
-        continue_shopping
-            This is a method to define continue shopping button
-        checkout
-            This is a method to define checkout button
+        click_finish
+            This is a method to define click FINISH button
+        click_cancel
+            This is a method to define click CANCEL button
         """
         super().__init__(driver)
 
     def click_finish(self) -> None:
-        """This is a method to find and click CONTINUE button on page."""
-        self.finish_button = self.driver.find_element(By.XPATH,
-                                        FINISH)
+        """This is a method to find and click FINISH button on page."""
+        self.finish_button = self.driver.find_element(
+            By.XPATH, FINISH)
         self.finish_button.click()
 
     def click_cancel(self) -> None:
         """This is a method to find and click CANCEL button on page."""
-        self.cancel_button = self.driver.find_element(By.XPATH,
-                                        CHP_2_CANCEL)
+        self.cancel_button = self.driver.find_element(
+            By.XPATH, CHP_2_CANCEL)
         self.cancel_button.click()
