f2d5854555d45878dd5e80e06d837da8318663c1
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-25 15:30:29 +0300

    pycodestyle fix

--- my_tests_1/pages/product_page.py
+++ my_tests_1/pages/product_page.py
@@ -4,6 +4,7 @@ from my_tests_1.pages.main_page import MainPage
 from selenium.common.exceptions import ElementClickInterceptedException
 from time import sleep
 
+
 class ProductPage(MainPage):
     """
     Constructs all the necessary attributes for the ProductPage object.
@@ -13,16 +14,19 @@ class ProductPage(MainPage):
     Methods:
     __init__
         This is a method to initialize instance of the LoginPage class.
-    
+    go_to_pdp
+        This is a method to define how to move to pdp
     add_to_cart_pdp
         This is a method to add item to the cart
-    back_button_pdp
-        This is a method to define the 'back' button.
+    remove_pdp
+        This method defines removing item from cart through pdp
+    back_pdp
+        This is a method to define the BACK button.
     """
-    
+
     def __init__(self, driver):
         """
-        This is a method to initialize instance of the MainPage class.
+        This is a method to initialize instance of the ProductPage class.
         Parameters
         webdriver:object
                 webdriver to initialize
@@ -31,20 +35,20 @@ class ProductPage(MainPage):
 
     def go_to_pdp_link(self, locator):
         """This is a method to define how to move to pdp."""
-        self.inventory_item_name = self.driver.find_element(By.ID, locator) 
+        self.inventory_item_name = self.driver.find_element(By.ID, locator)
         self.inventory_item_name.click()
-    
+
     def add_to_cart_pdp(self, locator):
         """This method defines adding product to cart from pdp."""
-        add_to_cart_button = self.driver.find_element(By.XPATH,locator)
-        add_to_cart_button.click()    
+        add_to_cart_button = self.driver.find_element(By.XPATH, locator)
+        add_to_cart_button.click()
 
     def remove_pdp(self, locator):
-        """This method defines removing item from cart through pdp.""" 
-        remove_button = self.driver.find_element(By.XPATH, locator)   
+        """This method defines removing item from cart through pdp."""
+        remove_button = self.driver.find_element(By.XPATH, locator)
         remove_button.click()
-    
+
     def back_pdp(self, locator):
-        """This method defines back button functionality of pdp. """    
+        """This method defines back button functionality of pdp. """
         self.back_button_pdp = self.driver.find_element(By.XPATH, locator)
         self.back_button_pdp.click()
