f644dca20337a7b2e9efc0338de2f17e4659fd22
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-25 15:31:26 +0300

    removed unneccesery imports

--- my_tests_1/pages/product_page.py
+++ my_tests_1/pages/product_page.py
@@ -1,8 +1,6 @@
 from selenium.webdriver.common.by import By
 from my_tests_1.lib.constants import *
 from my_tests_1.pages.main_page import MainPage
-from selenium.common.exceptions import ElementClickInterceptedException
-from time import sleep
 
 
 class ProductPage(MainPage):
