2905adbf219aaedef87aaf5b17005650f06dca58
Roman Kulyk <roman.kulyk@globallogic.com>
2024-08-25 15:16:44 +0300

    added annotations to the InventoryPage's methods

--- my_tests_1/pages/inventory_page.py
+++ my_tests_1/pages/inventory_page.py
@@ -43,49 +43,49 @@ class InventoryPage(MainPage):
         """
         super().__init__(driver)
 
-    def go_to_pdp_link(self, locator):
+    def go_to_pdp_link(self, locator: str) -> None:
         """This is a method to define how to move to pdp."""
         self.inventory_item_name = self.driver.find_element(By.XPATH, locator)
         self.inventory_item_name.click()
 
-    def add_to_cart_pdp(self, locator):
+    def add_to_cart_pdp(self, locator str) -> None:
         """This method defines adding product to cart from pdp."""
         add_to_cart_button = self.driver.find_element(By.XPATH, locator)
         add_to_cart_button.click()
 
-    def remove_pdp(self, locator):
+    def remove_pdp(self, locator str) -> None:
         """This method defines removing item from cart through pdp."""
         remove_button = self.driver.find_element(By.XPATH, locator)
         remove_button.click()
 
-    def sc_button(self):
+    def sc_button(self) -> None:
         """This is a method to define how to move to cart."""
         self.sc_container = self.driver.find_element(By.XPATH, CART_BUTTON)
         self.sc_container.click()
 
-    def sc_number(self):
+    def sc_number(self) -> None:
         """This is a method to define quantity of products in cart."""
         self.sc_badge = self.driver.find_element(By.XPATH, SC_BADGE)
         text = self.sc_badge.text
         return int(text)
 
-    def bm_button(self):
+    def bm_button(self) -> None:
         """This is a method to define bm_button."""
         self.bur_m_button = self.driver.find_element(By.XPATH, BM_BUTTON)
         self.bur_m_button.click()
 
-    def bm_cross_button(self):
+    def bm_cross_button(self) -> None:
         """This is a method to define bm_cross_button."""
         self.bur_cross_button = self.driver.find_element(
            By.XPATH, BM_CROSS_BUTTON)
         self.bur_cross_button.click()
 
-    def bm_item(self, text):
+    def bm_item(self, text: str) -> None:
         """This is a method to define bm items."""
         self.bur_item = self.driver.find_element(By.ID, text)
         self.bur_item.click()
 
-    def sort_item(self, locator):
+    def sort_item(self, locator: str) -> None:
         """This is a method to define sorting functionality."""
         self.sort_menu_item = self.driver.find_element(By.XPATH, locator)
         self.sort_menu_item.click()
