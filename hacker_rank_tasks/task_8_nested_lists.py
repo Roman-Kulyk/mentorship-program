0fbe86665d693b6fb3dc2c3d9ef426e4854f7e00
Roman Kulyk <roman.kulyk@globallogic.com>
2024-10-17 21:19:22 +0300

    addes task description

--- hacker_rank_tasks/task_8_nested_lists.py
+++ hacker_rank_tasks/task_8_nested_lists.py
@@ -3,7 +3,8 @@ Given the names and grades for each student in a class of N students, store them
 in a nested list and print the name(s) of any student(s) having the second lowest
 grade.
 Note: If there are multiple students with the second lowest grade, order their
-names alphabetically and pring each name on a new line."""
+names alphabetically and pring each name on a new line.
+"""
 
 if __name__ == '__main__':
 
@@ -13,9 +14,9 @@ if __name__ == '__main__':
     if 2 <= n <= 5:
         for _ in range(n):
             name = input("Enter student's name:")
-            scope = float(input("Enter student's grade:"))
-            python_students.append([name,scope])
-        
+            scope = float(input("Enter student's grade: "))
+            python_students.append([name, scope])
+
         second = max(python_students, key=lambda x: x[1])[1]
         first = min(python_students, key=lambda x: x[1])[1]
 
