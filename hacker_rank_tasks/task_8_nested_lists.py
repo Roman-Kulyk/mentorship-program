98a41c9d7a9411a8bc4cf0c148c953035d9b289b
RomaKulyk <36240408+RomaKulyk@users.noreply.github.com>
2024-10-27 11:15:50 +0200

    Update task_8_nested_lists.py, fix fthe issue with resulst print

--- hacker_rank_tasks/task_8_nested_lists.py
+++ hacker_rank_tasks/task_8_nested_lists.py
@@ -3,18 +3,18 @@ Given the names and grades for each student in a class of N students, store them
 in a nested list and print the name(s) of any student(s) having the second lowest
 grade.
 Note: If there are multiple students with the second lowest grade, order their
-names alphabetically and pring each name on a new line.
+names alphabetically and print each name on a new line.
 """
 
 if __name__ == '__main__':
 
     python_students = []
 
-    n = (int(input('Enter number of students in class 2 <= N <= 5:')))
+    n = (int(input()))
     if 2 <= n <= 5:
         for _ in range(n):
-            name = input("Enter student's name:")
-            scope = float(input("Enter student's grade: "))
+            name = input()
+            scope = float(input())
             python_students.append([name, scope])
 
         second = max(python_students, key=lambda x: x[1])[1]
@@ -24,6 +24,12 @@ if __name__ == '__main__':
             if python_students[i][1] <= second and python_students[i][1] != first:
                 second = python_students[i][1]
 
+        list_second_results = []
         for i in range(len(python_students)):
             if second == python_students[i][1]:
-                print(python_students[i][0])
+                
+                list_second_results.append(python_students[i][0])
+        sorted_name_list = (sorted(list_second_results))
+        for i in sorted_name_list:
+            print(i)
+            
