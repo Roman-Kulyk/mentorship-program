98cb74877563aa9b2fe67ce82f339f519e0c0b0b
Roman Kulyk <roman.kulyk@globallogic.com>
2024-11-01 16:59:31 +0200

    added task_12_text_wrap.py file

--- hacker_rank_tasks/task_12_text_wrap.py
+++ hacker_rank_tasks/task_12_text_wrap.py
@@ -21,10 +21,10 @@ Constraints
 import textwrap
 
 def wrap(string, max_width):
-    text = textwrap.wrap(string, max_width)
-    return text
+    result = textwrap.wrap(string, max_width)
+    for i in result:
+        print(i, sep = ' ')
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
-    print(result)
