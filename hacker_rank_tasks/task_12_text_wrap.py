8bfbf63359915154ac55b5387934bd01ebc2978f
RomaKulyk <36240408+RomaKulyk@users.noreply.github.com>
2024-11-02 11:40:07 +0200

    Update task_12_text_wrap.py

--- hacker_rank_tasks/task_12_text_wrap.py
+++ hacker_rank_tasks/task_12_text_wrap.py
@@ -22,9 +22,9 @@ import textwrap
 
 def wrap(string, max_width):
     result = textwrap.wrap(string, max_width)
-    for i in result:
-        print(i, sep = ' ')
+    return "\n".join(result)
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
+    print(result)
