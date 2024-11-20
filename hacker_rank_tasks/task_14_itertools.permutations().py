edccab701bcd535e95019d185bbd3c541b5ef3d2
Roman Kulyk <roman.kulyk@globallogic.com>
2024-11-20 20:49:22 +0200

    added solution to the file

--- hacker_rank_tasks/task_14_itertools.permutations().py
+++ hacker_rank_tasks/task_14_itertools.permutations().py
@@ -1,16 +1,18 @@
 """
 This tool returns successive  length permutations of elements in an iterable.
-
-If  is not specified or is None, then  defaults to the length of the iterable, and all possible full length permutations are generated.
-
-Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
+If  is not specified or is None, then  defaults to the length of the iterable,
+and all possible full length permutations are generated.
+Permutations are printed in a lexicographic sorted order.
+So, if the input iterable is sorted, the permutation tuples will be produced in
+a sorted order.
 Task
 You are given a string S.
-Your task is to print all possible permutations of size  of the string in lexicographic sorted order.
+Your task is to print all possible permutations of size  of the string in
+lexicographic sorted order.
 Input Format
-A single line containing the space separated string  and the integer value .
+A single line containing the space separated string  and the integer value k.
 Constraints
-0<k<=len(S)
+0< k <=len(S)
 The string contains only UPPERCASE characters.
 Output Format
 Print the permutations of the string  on separate lines.
@@ -30,6 +32,25 @@ KA
 KC
 KH
 Explanation
-All possible size 2 permutations of the string "HACK" are printed in lexicographic sorted order.
+All possible size 2 permutations of the string "HACK"
+are printed in lexicographic sorted order.
 """
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import permutations
+input_string = input().split()
+arr = input_string[0]
+r = int(input_string[1])
+
+def count_substring(arr, r):
+    # return list(sorted(permutations(arr, r)))
+    for i in list(sorted(permutations(arr, r))):
+        print(i[0] + i[1])
+
+count_substring(arr, r)
+
+# if __name__=="__main__":
+#     input_string = input().split()
+#     arr = input_string[0]
+#     r = int(input_string[1])
+#     print(count_substring(arr, r))
