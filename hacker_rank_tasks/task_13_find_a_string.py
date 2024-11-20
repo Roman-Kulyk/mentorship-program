a8974e241996c92e3296f02f86c112115f1df631
Roman Kulyk <roman.kulyk@globallogic.com>
2024-11-20 16:57:07 +0200

    added solution to the file

--- hacker_rank_tasks/task_13_find_a_string.py
+++ hacker_rank_tasks/task_13_find_a_string.py
@@ -29,10 +29,15 @@ A range function is used to loop over some length:
 range (0, 5)
 Here, the range loops over 0 to 4. 
 5 is excluded.
-""""
-def count_substring(string, sub_string):
-    return
+"""
 
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:i+(len(sub_string))] == sub_string:
+            count += 1
+    return count
+        
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
