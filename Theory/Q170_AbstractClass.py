6659eda9b91f43635edcbcaa513c9cbeb7f5a779
Roman-Kulyk <57181184+Roman-Kulyk@users.noreply.github.com>
2025-04-12 12:02:00 +0300

    Update Q170_AbstractClass.py

--- Theory/Q170_AbstractClass.py
+++ Theory/Q170_AbstractClass.py
@@ -1,92 +1,39 @@
-"""Abstract classes are classes than contain one or more abstract methods. An
-abstract method is a method that is declared, but contains no implementation.
-Abstract classes cannot be instantiated, and require subclasses to provide
-implementations for the abstract methods.
 """
+https://www.datacamp.com/tutorial/python-abstract-classes
+What is an Abstract Class?
+An abstract class is like a template for other classes. It defines methods that must 
+be included in any class that inherits from it, but it doesn’t provide the actual 
+code for those methods.
 
+Think of it as a recipe without specific ingredients—it tells you what steps to follow,
+but the details depend on the subclass.
 
-# class AbstractClass:
+Example:
+Let’s say you’re building a program to calculate the area of different shapes.
 
-#     def do_something(self):
-#         pass
-
-
-# class B(AbstractClass):
-#     pass
-
-
-# a = AbstractClass()
-# b = B()
-"""In fact, Python on its own doesn't provide abstract classes. Yet, Python
-comes with a module which provides the infrastructure for defining Abstract
-Base Classes(ABCs). This module is called - for obvious reasons -abc.
+You create an abstract class called Shape that says every shape must have an area() method.
+But Shape doesn’t define how area() works—because the formula depends on the type of shape.
+Each specific shape (like a Circle or Rectangle) inherits from Shape and provides its own
+version of area().
 """
 from abc import ABC, abstractmethod
 
-
-class AbstrctClassExample(ABC):
-
-    def __init__(self, value):
-        self.value = value
-        super().__init__()
-
+class Vehicle(ABC):
     @abstractmethod
-    def do_something(self):
+    def start_engine(self):
         pass
 
-# class DoAdd42(AbstrctClassExample):
-#     pass
-
-# x = DoAdd42(4)
-
-# # We get an exception that DoAdd42 can't be instantiated.
-# # We will do it in correct way in the following example, in which we define
-# # two classes inheriting from our abstract class
-
-
-class DoAdd42(AbstrctClassExample):
-
-    def do_something(self):  # this is an example of polymorphism...
-        return self.value + 42
-
-
-class DoMu142(AbstrctClassExample):
-
-    def do_something(self):  # we have two methods with the same name^^^
-        # doing different things
-        return self.value * 42
-
-# A class that is derived from an abstract class cannot be instantiated unless
-# all of its abstract methods are overriden.
-
-
-x = DoAdd42(10)
-
-
-y = DoMu142(10)
-
-print(x.do_something())
-print(y.do_something())
-
-###############################################################################
-
-
-class AbstractClassExample(ABC):
-
     @abstractmethod
-    def do_something(self):
-        print('Some implementation!')
-# An abstract- method can have an implementation in the abstract class! Even
-# if they are implemented, designers of subclassed will be forced to override
-# the iplementation.
+    def stop_engine(self):
+        pass
 
+class Car(Vehicle):
+    def start_engine(self):
+        print("Car engine started")
 
-class AnotherSubclass(AbstractClassExample):
+    def stop_engine(self):
+        print("Car engine stopped")
 
-    def do_something(self):
-        super().do_something()
-        print('The enrichment from AnotherSubclass')
-
-
-x = AnotherSubclass()
-x.do_something()
+car = Car()
+car.start_engine()
+car.stop_engine()
